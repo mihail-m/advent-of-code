f1426119f90ad66e1f68e0f4c35f238ee74c83cf
Mihail Mihaylov <mihailm@vmware.com>
2022-12-15 19:01:39 +0200

    Refactor solution for day 14 task.

--- src/main/java/aoc/Day14Task.java
+++ src/main/java/aoc/Day14Task.java
@@ -1,7 +1,7 @@
 package aoc;
 
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import aoc.base.Task;
 
@@ -17,70 +17,58 @@ public class Day14Task extends Task<List<StringBuilder>, Integer> {
         FIND_FALLEN_UNITS_OF_SAND {
             @Override
             public void solve(Day14Task task) {
-                task.result = findSand(task.input, false);
+                findSand(task, false);
             }
         },
 
         FIND_UNITS_OF_SAND_WITH_FLOOR {
             @Override
             public void solve(Day14Task task) {
-                task.result = findSand(task.input, true);
+                findSand(task, true);
             }
         };
 
         private static final Node SAND_HOLE = new Node(0, 500);
 
-        private static int findSand(List<StringBuilder> cave, boolean whenNotOnMap) {
-            int ans = 0;
+        private static void findSand(Day14Task task, boolean hasFloor) {
+            AtomicBoolean onMap = new AtomicBoolean(true);
 
-            while (true) {
-                boolean onMap = true;
+            while (onMap.get()) {
+                Node node = sandFall(task.input, SAND_HOLE.copy(), null, onMap, hasFloor);
 
-                Node prev = null;
-                Node cur = new Node(SAND_HOLE.row, SAND_HOLE.col);
-
-                while (!cur.equals(prev)) {
-                    prev = cur;
-
-                    for (Node.Move move : Node.Move.values()) {
-                        Node newNode = cur.doMove(move);
-
-                        if (!getInBoundsPredicate(cave).test(newNode)) {
-                            onMap = whenNotOnMap;
-                            if (!onMap) {
-                                break;
-                            }
-                            continue;
-                        }
-                        if (cave.get(newNode.row).charAt(newNode.col) != '.') {
-                            continue;
-                        }
-
-                        cur = newNode;
-                        break;
-                    }
+                if (onMap.get()) {
+                    task.result++;
+                    task.input.get(node.row).setCharAt(node.col, 'o');
                 }
 
-                if (onMap) {
-                    ans++;
-                    cave.get(cur.row).setCharAt(cur.col, 'o');
-                } else {
-                    break;
-                }
-
-                if (cur.col == SAND_HOLE.col && cur.row == SAND_HOLE.row) {
+                if (node.at(SAND_HOLE)) {
                     break;
                 }
             }
-
-            return ans;
         }
 
-        private static Predicate<Node> getInBoundsPredicate(List<StringBuilder> map) {
-            return (node) -> node.row >= 0
-                    && node.col >= 0
-                    && node.row < map.size()
-                    && node.col < map.get(0).length();
+        private static Node sandFall(List<StringBuilder> cave, Node cur, Node prev, AtomicBoolean onMap, boolean hasFloor) {
+            if (!cur.equals(prev)) {
+                for (Node.Move move : Node.Move.values()) {
+                    Node newNode = cur.doMove(move);
+                    if (!validMove(cave, newNode, onMap, hasFloor) && onMap.get()) {
+                        continue;
+                    }
+                    if (onMap.get()) {
+                        return sandFall(cave, newNode, cur, onMap, hasFloor);
+                    }
+                    break;
+                }
+            }
+            return cur;
+        }
+
+        private static boolean validMove(List<StringBuilder> cave, Node node, AtomicBoolean onMap, boolean hasFloor) {
+            if (node.row >= 0 && node.col >= 0 && node.row < cave.size() && node.col < cave.get(0).length()) {
+                return cave.get(node.row).charAt(node.col) == '.';
+            }
+            onMap.set(hasFloor);
+            return false;
         }
 
         private static class Node {
@@ -107,6 +95,14 @@ public class Day14Task extends Task<List<StringBuilder>, Integer> {
 
             public Node doMove(Node.Move move) {
                 return new Node(this.row + move.row, this.col + move.col);
+            }
+
+            public Node copy() {
+                return new Node(this.row, this.col);
+            }
+
+            public boolean at(Node node) {
+                return this.row == node.row && this.col == node.col;
             }
         }
     }
