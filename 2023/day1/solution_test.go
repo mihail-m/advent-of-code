0792f06ba813a6e1c05cc4834d38e86861d32759
Mihail Mihaylov <momodei.misho@gmail.com>
2023-12-01 09:06:17 +0200

    Day1 task

--- 2023/day1/solution_test.go
+++ 2023/day1/solution_test.go
@@ -1,25 +1,38 @@
 package day1
 
 import (
+	"bytes"
+	"fmt"
+	"os"
 	"testing"
 )
 
 func TestTask1SampleInput(t *testing.T) {
-
+	fmt.Println(readInput("sample_input"))
 }
 
-func TestTask1(t *testing.T) {
+// func TestTask1(t *testing.T) {
 
-}
+// }
 
-func TestTask2SampleInput(t *testing.T) {
+// func TestTask2SampleInput(t *testing.T) {
 
-}
+// }
 
-func TestTask2(t *testing.T) {
+// func TestTask2(t *testing.T) {
 
-}
+// }
 
-func readInput() {
+func readInput(fileName string) []string {
+	body, err := os.ReadFile("input/" + fileName + ".txt")
+	if err != nil {
+		fmt.Printf("unable to read file: %v", err)
+	}
+	lines := bytes.Split(body, []byte(" "))
 
+	result := make([]string, len(lines))
+	for _, line := range lines {
+		result = append(result, string(line))
+	}
+	return result
 }
