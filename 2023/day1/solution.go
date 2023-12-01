7a30bfb2c641598cc834645f7beddff0fc54359d
Mihail Mihaylov <mihailm@vmware.com>
2023-12-01 10:30:26 +0200

    Add day1 solution

--- 2023/day1/solution.go
+++ 2023/day1/solution.go
@@ -1,5 +1,41 @@
 package day1
 
-func Solve() {
+import (
+	"unicode"
+)
 
+func Solve1(input []string) int {
+	res := 0
+	for _, line := range input {
+		dig := make([]int, 0)
+		for _, r := range line {
+			if unicode.IsDigit(r) {
+				dig = append(dig, int(r-'0'))
+			}
+		}
+		res += dig[0]*10 + dig[len(dig)-1]
+	}
+	return res
+}
+
+func Solve2(input []string) int {
+	res := 0
+	digStr := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+	for _, line := range input {
+		digs := make([]int, 0)
+		for i, r := range line {
+			if unicode.IsDigit(r) {
+				digs = append(digs, int(r-'0'))
+				continue
+			}
+			for v, dig := range digStr {
+				if i+len(dig) <= len(line) && line[i:i+len(dig)] == dig {
+					digs = append(digs, v+1)
+					break
+				}
+			}
+		}
+		res += digs[0]*10 + digs[len(digs)-1]
+	}
+	return res
 }
--- 2023/day1/solution_test.go
+++ 2023/day1/solution_test.go
@@ -8,31 +8,35 @@ import (
 )
 
 func TestTask1SampleInput(t *testing.T) {
-	fmt.Println(readInput("sample_input"))
+	if result := Solve1(readInput("sample_input1.txt")); result != 142 {
+		t.Errorf("Expected 142 but got %d", result)
+	}
 }
 
-// func TestTask1(t *testing.T) {
+func TestTask1(t *testing.T) {
+	fmt.Println(Solve1(readInput("input.txt")))
+}
 
-// }
+func TestTask2SampleInput(t *testing.T) {
+	if result := Solve2(readInput("sample_input2.txt")); result != 281 {
+		t.Errorf("Expected 281 but got %d", result)
+	}
+}
 
-// func TestTask2SampleInput(t *testing.T) {
-
-// }
-
-// func TestTask2(t *testing.T) {
-
-// }
+func TestTask2(t *testing.T) {
+	fmt.Println(Solve2(readInput("input.txt")))
+}
 
 func readInput(fileName string) []string {
-	body, err := os.ReadFile("input/" + fileName + ".txt")
+	body, err := os.ReadFile("input/" + fileName)
 	if err != nil {
 		fmt.Printf("unable to read file: %v", err)
 	}
-	lines := bytes.Split(body, []byte(" "))
+	lines := bytes.Split(body, []byte("\n"))
 
 	result := make([]string, len(lines))
-	for _, line := range lines {
-		result = append(result, string(line))
+	for i, line := range lines {
+		result[i] = string(line)
 	}
 	return result
 }
